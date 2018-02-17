2c302cfe2f4152f780ce1bde50605ebfa86efbd3
Martin Dieblich <martin.dieblich@gmx.de>
2018-02-17 16:40:11 +0100

    Nummer der Frage vor Frage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { QuestionComponent } from './question/question.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: AppComponent},
-  { path: 'question/:id', component: QuestionComponent},
+  { path: 'question/:id', component: QuestionComponent}
 ];
 
 @NgModule({
